7e09b4f8c774590c2c8661b0ca246836b0705d04
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-08-25 14:10:13 +0100

    feat(web): add Header, Footer, Outlet, Grid to root app.jsx

--- apps/web/src/app.jsx
+++ apps/web/src/app.jsx
@@ -1,5 +1,18 @@
+import { Grid, Heading } from "@radix-ui/themes";
+import { Outlet } from "react-router";
+import Header from "./components/header";
+import Footer from "./components/footer";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Grid columns={"1"} rows={"auto 1fr auto"} height={"100vh"}>
+        <Header />
+        <Outlet />
+        <Footer />
+      </Grid>
+    </>
+  );
 };
 
 export default App;
