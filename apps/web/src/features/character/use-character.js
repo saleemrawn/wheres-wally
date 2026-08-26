d86adbc0a07e8fbddac8e05e2a5b0042f968bad0
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-08-26 16:12:26 +0100

    feat(web): update useCharacter with getCharacters API call

--- apps/web/src/features/character/use-character.js
+++ apps/web/src/features/character/use-character.js
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as service from "./character-service.js";
 
 const useCharacter = () => {
   const [characters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const getCharacters = async () => {
+    setIsLoading(true);
+    try {
+      const characters = await service.getCharacters();
+      setCharacters(characters.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
 
   return { characters, isLoading, error };
 };
