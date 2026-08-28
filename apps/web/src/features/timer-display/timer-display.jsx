8814bc9f9b5dc185228c0593caff65165d375713
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-08-28 14:18:51 +0100

    feat(web): add TimerDisplay component & useTimerDisplay custom hook

---
+++ apps/web/src/features/timer-display/timer-display.jsx
@@ -0,0 +1,32 @@
+import { Button, Flex, Text } from "@radix-ui/themes";
+
+const format = (num) => {
+  return (num + "").length === 1 ? "0" + num : num + "";
+};
+
+const TimerDisplay = ({ time }) => {
+  return (
+    <Flex
+      direction={"column"}
+      justify={"center"}
+      align={"center"}
+      py={{ initial: "2", xs: "4" }}
+      className="w-full border-r-2 border-r-white"
+    >
+      <Text className="font-sniglet text-white text-xl md:text-3xl">Time</Text>
+      <Flex>
+        <Text className="font-semibold text-white text-xl md:text-3xl">
+          {format(time.hh)}:
+        </Text>
+        <Text className="font-semibold text-white text-xl md:text-3xl">
+          {format(time.mm)}:
+        </Text>
+        <Text className="font-semibold text-white text-xl md:text-3xl">
+          {format(time.ss)}
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default TimerDisplay;
---
+++ apps/web/src/features/timer-display/use-timer-display.js
@@ -0,0 +1,41 @@
+import { useCallback, useRef, useState } from "react";
+
+const useTimerDisplay = () => {
+  const [time, setTime] = useState({ hh: 0, mm: 0, ss: 0, ms: 0, total: 0 });
+  const intervalRef = useRef(null);
+
+  const startTimer = useCallback(() => {
+    if (intervalRef.current) return;
+
+    intervalRef.current = setInterval(() => {
+      setTime((prev) => {
+        let { hh, mm, ss, ms, total } = prev;
+        total++;
+        ms++;
+
+        if (ms >= 100) {
+          ss++;
+          ms = 0;
+        }
+        if (ss >= 60) {
+          mm++;
+          ss = 0;
+        }
+        if (mm >= 60) {
+          hh++;
+          mm = 0;
+        }
+        return { hh, mm, ss, ms, total };
+      });
+    }, 10);
+  }, []);
+
+  const endTimer = useCallback(() => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+  }, []);
+
+  return { time, startTimer, endTimer };
+};
+
+export default useTimerDisplay;
