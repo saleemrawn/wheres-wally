1fc0f86cb9182b9fc1522c017041ec64de6efa15
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-08-28 11:44:03 +0100

    feat(web): add context provider to manage game state

---
+++ apps/web/src/context/game-state.jsx
@@ -0,0 +1,35 @@
+import { createContext, useCallback, useContext, useState } from "react";
+
+const GameStateContext = createContext();
+const GameStateUpdateContext = createContext();
+
+const useGameState = () => {
+  return useContext(GameStateContext);
+};
+
+const useGameStateUpdate = () => {
+  return useContext(GameStateUpdateContext);
+};
+
+const GameStateProvider = ({ children }) => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
+
+  const setGameRunning = useCallback((bool) => {
+    setIsRunning(bool);
+  }, []);
+
+  const setGameFinished = useCallback((bool) => {
+    setIsFinished(bool);
+  }, []);
+
+  return (
+    <GameStateContext value={{ isRunning, isFinished }}>
+      <GameStateUpdateContext value={{ setGameRunning, setGameFinished }}>
+        {children}
+      </GameStateUpdateContext>
+    </GameStateContext>
+  );
+};
+
+export { GameStateProvider, useGameState, useGameStateUpdate };
