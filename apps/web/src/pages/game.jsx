908f34098f8983b9c452fc89aa3f01cef86adfc0
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-09-02 15:55:07 +0100

    feat(web): add CharacterDialog, CharacterList to Game component
    - destructure isOpen, openDialog, closeDialog from useDialog custom hook
    - pass prop openDialog to Viewer to open character selector on user click
    - pass prop isOpen, closeDialog to CharacterDialog

--- apps/web/src/pages/game.jsx
+++ apps/web/src/pages/game.jsx
@@ -1,18 +1,22 @@
 import { useEffect } from "react";
 import { useGameStateUpdate } from "../context/game-state";
 import { Viewer } from "../features/viewer/viewer";
+import { CharacterDialog } from "../features/character-select/character-dialog";
+import { CharacterList } from "../features/character-select/character-list";
 import { TimerDisplay } from "../features/timer-display/timer-display";
 import { useTimerDisplay } from "../features/timer-display/use-timer-display";
 import { RoundsDisplay } from "../features/rounds-display/rounds-display";
 import { useRoundsDisplay } from "../features/rounds-display/use-rounds-display";
-import { Flex, Skeleton } from "@radix-ui/themes";
 import { useViewer } from "../features/viewer/use-viewer";
+import { useDialog } from "../hooks/use-dialog";
+import { Flex, Skeleton } from "@radix-ui/themes";
 
 const Game = () => {
   const { setGameRunning, setGameFinished } = useGameStateUpdate();
   const { time, startTimer, endTimer } = useTimerDisplay();
   const { currentRound, totalRounds } = useRoundsDisplay();
   const { image, isLoading } = useViewer(currentRound);
+  const { isOpen, openDialog, closeDialog } = useDialog();
 
   useEffect(() => {
     setGameRunning(true);
@@ -22,13 +26,17 @@ const Game = () => {
   return (
     <>
       <Skeleton loading={isLoading}>
-        <Viewer token={image?.imageToken} />
+        <Viewer token={image?.imageToken} onClick={openDialog} />
       </Skeleton>
 
       <Flex className="bg-red-500">
         <TimerDisplay time={time} />
         <RoundsDisplay currentRound={currentRound} totalRounds={totalRounds} />
       </Flex>
+
+      <CharacterDialog isOpen={isOpen} onClose={closeDialog}>
+        <CharacterList />
+      </CharacterDialog>
     </>
   );
 };
