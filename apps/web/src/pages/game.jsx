9f8241625296f95358e9a6eaab81d2c52bf85d82
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-09-02 09:37:36 +0100

    feat(web): add TimerDisplay, RoundsDisplay to Game component
    - destructure time, startTimer, endTimer from useTimerDisplay custom hook
    - pass time prop to TimerDisplay
    - call startTimer in useEffect, timer starts when Game component is rendered
    - destructure currentRound, totalRounds from useRoundsDisplay custom hook
    - pass currentRound, totalRounds props to RoundsDisplay

--- apps/web/src/pages/game.jsx
+++ apps/web/src/pages/game.jsx
@@ -1,14 +1,29 @@
 import { useEffect } from "react";
 import { useGameStateUpdate } from "../context/game-state";
+import { Flex } from "@radix-ui/themes";
+import TimerDisplay from "../features/timer-display/timer-display";
+import useTimerDisplay from "../features/timer-display/use-timer-display";
+import RoundsDisplay from "../features/rounds-display/rounds-display";
+import useRoundsDisplay from "../features/rounds-display/use-rounds-display";
 
 const Game = () => {
   const { setGameRunning, setGameFinished } = useGameStateUpdate();
+  const { time, startTimer, endTimer } = useTimerDisplay();
+  const { currentRound, totalRounds } = useRoundsDisplay(5);
 
   useEffect(() => {
     setGameRunning(true);
+    startTimer();
   }, []);
 
-  return <></>;
+  return (
+    <>
+      <Flex className="bg-red-500">
+        <TimerDisplay time={time} />
+        <RoundsDisplay currentRound={currentRound} totalRounds={totalRounds} />
+      </Flex>
+    </>
+  );
 };
 
 export default Game;
