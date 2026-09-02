a9b9a031053c450076bd744d7a682f17ce9d2e90
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-09-02 16:29:45 +0100

    feat(web): add Radix UI Form to CharacterDialog
    - add onSubmit, children prop to CharacterDialog, CharacterList will be rendered as children element

--- apps/web/src/features/character-select/character-dialog.jsx
+++ apps/web/src/features/character-select/character-dialog.jsx
@@ -1,7 +1,8 @@
 import { Dialog, Button, Flex } from "@radix-ui/themes";
+import { Form } from "radix-ui";
 import { X } from "lucide-react";
 
-const CharacterDialog = ({ isOpen, onClose }) => {
+const CharacterDialog = ({ isOpen, onClose, onSubmit, children }) => {
   return (
     <Dialog.Root open={isOpen}>
       <Dialog.Content className="max-w-240! p-10!">
@@ -22,6 +23,23 @@ const CharacterDialog = ({ isOpen, onClose }) => {
         <Dialog.Description mb={"4"}>
           Pick a character to check if your selection is correct:
         </Dialog.Description>
+
+        <Form.Root
+          onSubmit={(event) => {
+            event.preventDefault();
+            onSubmit();
+          }}
+        >
+          <Form.Field name="character" className="mb-4">
+            {children}
+          </Form.Field>
+
+          <Form.Submit asChild>
+            <Button size={"3"} radius={"full"}>
+              Submit
+            </Button>
+          </Form.Submit>
+        </Form.Root>
       </Dialog.Content>
     </Dialog.Root>
   );
