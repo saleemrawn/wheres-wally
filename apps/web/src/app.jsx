26ec55aadc3fee934f004e265109d467ced332bf
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-09-02 09:15:14 +0100

    feat(web): wrap context GameStateProvider in App share game state data

--- apps/web/src/app.jsx
+++ apps/web/src/app.jsx
@@ -1,16 +1,19 @@
 import { Grid, Heading } from "@radix-ui/themes";
 import { Outlet } from "react-router";
+import { GameStateProvider } from "./context/game-state";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
 const App = () => {
   return (
     <>
-      <Grid columns={"1"} rows={"auto 1fr auto"} height={"100vh"}>
-        <Header />
-        <Outlet />
-        <Footer />
-      </Grid>
+      <GameStateProvider>
+        <Grid columns={"1"} rows={"auto 1fr auto"} height={"100vh"}>
+          <Header />
+          <Outlet />
+          <Footer />
+        </Grid>
+      </GameStateProvider>
     </>
   );
 };
