8cd699ec4d697ea117ba295e8511aa84bed3e0a7
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-09-02 09:17:56 +0100

    feat(web): update game state in Game component
    - destructure setGameRunning, setGameFinished from useGameStateUpdate custom hook to update game states
    - call setGameRunning and pass 'true' in useEffect, game starts immediately once Game component is rendered

--- apps/web/src/pages/game.jsx
+++ apps/web/src/pages/game.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { useGameStateUpdate } from "../context/game-state";
+
 const Game = () => {
+  const { setGameRunning, setGameFinished } = useGameStateUpdate();
+
+  useEffect(() => {
+    setGameRunning(true);
+  }, []);
+
   return <></>;
 };
 
