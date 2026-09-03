77035ba6fbaf43c2211bd387e8361243129c60d2
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-09-03 15:22:52 +0100

    feat(web): add Toaster, ToastBar to App for custom toast notifications

--- apps/web/src/app.jsx
+++ apps/web/src/app.jsx
@@ -1,5 +1,6 @@
 import { Grid, Heading } from "@radix-ui/themes";
 import { Outlet } from "react-router";
+import { Toaster, ToastBar } from "react-hot-toast";
 import { GameStateProvider } from "./context/game-state";
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
@@ -7,6 +8,22 @@ import { Footer } from "./components/footer";
 const App = () => {
   return (
     <>
+      <Toaster
+        toastOptions={{
+          success: {
+            className:
+              "font-sniglet text-2xl w-full !bg-green-600 !text-white !rounded-lg",
+          },
+          error: {
+            className:
+              "font-sniglet text-2xl w-full !bg-red-600 !text-white !rounded-lg",
+          },
+        }}
+      >
+        {(t) => (
+          <ToastBar toast={t}>{({ message }) => <>{message}</>}</ToastBar>
+        )}
+      </Toaster>
       <GameStateProvider>
         <Grid columns={"1"} rows={"auto 1fr auto"} height={"100vh"}>
           <Header />
