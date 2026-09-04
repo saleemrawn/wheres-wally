69bc3213bbb2b955e6089995cc7a605f07b38e86
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-09-04 08:02:22 +0100

    feat(api): enable all CORS requests
    - add CORS middleware dependency
    - mount CORS middlelware in App

--- apps/api/app.js
+++ apps/api/app.js
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import * as routes from "./routes/routes.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/characters", routes.character);
