218c515d9953e26562e2ec5df1342d53df461f64
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-09-10 16:01:54 +0100

    feat(web): add addLeaderboardTime service function for API call

--- apps/web/src/features/leaderboard/leaderboard-service.js
+++ apps/web/src/features/leaderboard/leaderboard-service.js
@@ -7,9 +7,14 @@ const getLeaderboard = async () => {
   return res.data;
 };
 
+const addLeaderboardTime = async ({ name, time }) => {
+  const res = await api.post(basePath, { name, time });
+  return res.data;
+};
+
 const checkWithinTopTen = async (time) => {
   const res = await api.get(`${basePath}/${time}`);
   return res.data;
 };
 
-export { getLeaderboard, checkWithinTopTen };
+export { getLeaderboard, addLeaderboardTime, checkWithinTopTen };
