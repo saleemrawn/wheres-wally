4b1f5b51fd7488fe90bd3d53c627d800b75cef6e
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-09-11 09:25:38 +0100

    feat(web): render server errors & errors reset for form submission

--- apps/web/src/features/leaderboard/leaderboard-form.jsx
+++ apps/web/src/features/leaderboard/leaderboard-form.jsx
@@ -1,23 +1,45 @@
 import { useState } from "react";
-import { Flex, Button, TextField, Heading, Text } from "@radix-ui/themes";
+import {
+  Flex,
+  Button,
+  TextField,
+  Heading,
+  Text,
+  Callout,
+} from "@radix-ui/themes";
 import { Form } from "radix-ui";
 
-const LeaderboardForm = ({ onSubmit }) => {
+const LeaderboardForm = ({ errors, onSubmit, onResetErrors }) => {
   const [name, setName] = useState("");
 
   return (
     <Form.Root
       onSubmit={(event) => {
         event.preventDefault();
+        onResetErrors();
         onSubmit(name);
       }}
+      onClearServerErrors={onResetErrors}
       className="bg-gray-200 p-10 rounded-3xl"
     >
       <Flex direction={"column"} gap={"6"}>
         <Heading as="h2" className="text-center">
           ...and made the top 10 fastest times!
         </Heading>
-        <Form.Field name="name">
+
+        {errors ? (
+          <Flex direction={"column"} gap={"3"}>
+            {errors.map((err) => {
+              return (
+                <Callout.Root color="red" size={"1"} key={err.msg}>
+                  <Callout.Text>{err.msg}</Callout.Text>
+                </Callout.Root>
+              );
+            })}
+          </Flex>
+        ) : null}
+
+        <Form.Field name="name" serverInvalid={Boolean(errors)}>
           <Flex direction={"column"} gap={"2"} align={"center"}>
             <Form.Label className="text-center">
               Enter your name, nickname or initials
@@ -30,7 +52,10 @@ const LeaderboardForm = ({ onSubmit }) => {
                 size="3"
                 className="w-full"
                 value={name}
-                onChange={(event) => setName(event.target.value)}
+                onChange={(event) => {
+                  setName(event.target.value);
+                  onResetErrors();
+                }}
                 required
               />
             </Form.Control>
